326bf83e27091f78246a817797e48977209c9523
Jeskay <37577958+Jeskay@users.noreply.github.com>
2020-09-17 00:31:19 +0300

    Fuck it

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -2,6 +2,9 @@
 
 package lesson5.task1
 
+import java.lang.Integer.min
+import java.lang.Math.max
+
 // Урок 5: ассоциативные массивы и множества
 // Максимальное количество баллов = 14
 // Рекомендуемое количество баллов = 9
@@ -277,7 +280,17 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   findSumOfTwo(listOf(1, 2, 3), 4) -> Pair(0, 2)
  *   findSumOfTwo(listOf(1, 2, 3), 6) -> Pair(-1, -1)
  */
-fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> = TODO()
+fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
+    val arr = mutableMapOf<Int, Int>()
+    for ((counter, num) in list.withIndex()) {
+        if (arr.contains(number - num)) return Pair(
+            min(arr[number - num]!!, counter),
+            max(arr[number - num]!!, counter)
+        )
+        arr[num] = counter
+    }
+    return Pair(-1,-1)
+}
 
 /**
  * Очень сложная (8 баллов)
